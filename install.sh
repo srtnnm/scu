e2616cb7807669de829617e49532bae21027625a
Sk44rt <57320865+Sk44rt@users.noreply.github.com>
2023-06-07 01:03:52 +0000

    deleted install.sh
    4a5bf17 (https://github.com/Sk44rt/scu/commit/4a5bf172b59bd55437f3138380b9b47398e6bdbe) didn't removed this file.
    idk why so i'm removing from web browser

--- install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cargo build --release
-if [[ -f "target/release/scu" ]]; then
-  echo "Please write root password for installing program."
-  sudo mv target/release/scu /usr/bin
-else
-  echo "Compilation failed"
-fi
