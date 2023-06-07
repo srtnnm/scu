e8ff2b7658fdfd0555d8200e4746bf1804e9051b
geekylthyosaur <geekylthyosaur@gmail.com>
2023-06-07 14:14:49 +0300

    Fix detection of init system

--- src/software/init_system.rs
+++ src/software/init_system.rs
@@ -1,38 +1,24 @@
-use std::fs;
+use std::path::Path;
 
 pub fn detect() -> String {
-  let mut link: String = String::new();
+    let comm = std::fs::read_to_string("/proc/1/comm").unwrap_or("Unknown".into());
 
-  let buff = fs::read_to_string("/proc/1/cmdline").unwrap()
-    .as_str()
-    .replace('\0', "");
-  let mut buf = buff.as_str();
-
-  if buf.contains(' ') {
-    buf = buf.split(' ')
-      .next()
-      .unwrap();
-  }
-
-  let metadata = fs::symlink_metadata(buf);
-  if metadata.is_ok() &&
-    metadata.unwrap()
-      .file_type()
-      .is_symlink() {
-        link = fs::read_link(buf).unwrap()
-          .into_os_string()
-          .into_string()
-          .unwrap();
-  }
-
-  String::from( 
-    match link.as_str() {
-      "openrc-init"            => "OpenRC",
-      "/lib/systemd/systemd"   => "SystemD",
-      "../lib/systemd/systemd" => "SystemD",
-      "/lib/sysvinit/init"     => "SysVinit",
-      "../lib/sysvinit/init"   => "SysVinit",
-      _                        => "Unknown"
-    }
-  )
+    (match comm.trim() {
+        "systemd" => "Systemd",
+        "openrc-init" | "init-openrc" => "OpenRC",
+        "runit" => "Runit",
+        "init" => {
+            if Path::new("/run/dinit").exists() {
+                "Dinit"
+            } else if Path::new("/usr/share/sysvinit/inittab").exists() {
+                "SysVinit"
+            } else {
+                "Unknown"
+            }
+        },
+        "s6-svscan" => "s6",
+        "upstart" => "Upstart",
+        s => s,
+    })
+    .into()
 }
