42dd0e7fb00ecba894f9ecc6f1052fe910aae752
sk44rt <a641557@gmail.com>
2023-09-24 04:36:24 +0200

    Battery path detection fix

--- src/hardware/battery.rs
+++ src/hardware/battery.rs
@@ -19,16 +19,32 @@ pub fn get_battery_info() -> Option<BatteryInfo> {
         capacity: 0_u16,
     };
 
-    if Path::new("/sys/class/power_supply/BAT0").exists() {
-        result.model = match fs::read_to_string("/sys/class/power_supply/BAT0/model_name") {
+    let mut battery_dir = String::from("");
+
+    if Path::new("/sys/class/power_supply").exists() {
+        let power_supply = fs::read_dir("/sys/class/power_supply");
+        if let Ok(power_supply) = power_supply {
+            for dir in power_supply {
+                let dir = dir.unwrap().file_name().into_string().unwrap();
+                if dir.starts_with("BAT") && dir.len() == 4 {
+                    battery_dir = format!("/sys/class/power_supply/{}", dir);
+                    break;
+                }
+            }
+        }
+    }
+    if battery_dir.is_empty() { return None; }
+
+    if Path::new(&battery_dir).exists() {
+        result.model = match fs::read_to_string(format!("{}/model_name", battery_dir)) {
             Ok(content) => content.trim().to_string(),
             Err(_) => "Unknown".to_string(),
         };
-        result.technology = match fs::read_to_string("/sys/class/power_supply/BAT0/technology") {
+        result.technology = match fs::read_to_string(format!("{}/technology", battery_dir)) {
             Ok(content) => content.trim().to_string(),
             Err(_) => "Unknown".to_string(),
         };
-        result.capacity = match fs::read_to_string("/sys/class/power_supply/BAT0/capacity") {
+        result.capacity = match fs::read_to_string(format!("{}/capacity", battery_dir)) {
             Ok(content) => match content.trim().to_string().parse::<u16>() {
                 Ok(integer) => integer,
                 Err(_) => 0_u16,
