4cd85ad4dc1460956cc62c298945176ca6f901bf
sk44rt <a641557@gmail.com>
2024-04-17 02:53:41 +0200

    format build.rs

--- build.rs
+++ build.rs
@@ -9,13 +9,13 @@ fn get_metadata() -> Option<String> {
 
     Some(
         String::from_utf8(output.stdout)
-        .ok()?
-        .split("\"dependencies\":[{\"name\":\"libscu\",\"source\":\"registry+https://github.com/rust-lang/crates.io-index\",\"req\":\"^")
-        .nth(1)
-        .map(|s|s.to_string())?
-        .split("\"")
-        .next()
-        .map(|s|s.to_string())?
+            .ok()?
+            .split("\"dependencies\":[{\"name\":\"libscu\",\"source\":\"registry+https://github.com/rust-lang/crates.io-index\",\"req\":\"^")
+            .nth(1)
+            .map(|s|s.to_string())?
+            .split("\"")
+            .next()
+            .map(|s|s.to_string())?
     )
 }
 
