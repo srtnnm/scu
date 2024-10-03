77dad62f1cad456de5a865979e6d61994a89a5ad
sk44rt <a641557@gmail.com>
2024-10-03 13:49:50 +0200

    config.rs: replace table names string with bitflags, add default_config() and full_config() functions

--- src/config.rs
+++ src/config.rs
@@ -9,24 +9,81 @@ all tables:
     battery
 */
 
+use bitflags::bitflags;
 use libscu::software::users::fetch_current;
 use std::{fs, path::Path};
 
-const ALL_TABLES: [&str; 7] = [
-    "system",
-    "processor",
-    "memory",
-    "graphics",
-    "packages",
-    "disks",
-    "battery",
+use crate::info;
+
+bitflags! {
+    #[derive(PartialEq)]
+    struct Table: u8 {
+        const SYSTEM    = 0b0000001;
+        const PROCESSOR = 0b0000010;
+        const MEMORY    = 0b0000100;
+        const GRAPHICS  = 0b0001000;
+        const PACKAGES  = 0b0010000;
+        const DISKS     = 0b0100000;
+        const BATTERY   = 0b1000000;
+    }
+}
+
+impl Table {
+    pub fn from_str(str: &str) -> Option<Self> {
+        match str {
+            "system" => Some(Self::SYSTEM),
+            "processor" => Some(Self::PROCESSOR),
+            "memory" => Some(Self::MEMORY),
+            "graphics" => Some(Self::GRAPHICS),
+            "packages" => Some(Self::PACKAGES),
+            "disks" => Some(Self::DISKS),
+            "battery" => Some(Self::BATTERY),
+            "full" => Some(Self::all()),
+            _ => None,
+        }
+    }
+    pub fn to_str(&self) -> &'static str {
+        match *self {
+            Self::SYSTEM => "system",
+            Self::PROCESSOR => "processor",
+            Self::MEMORY => "memory",
+            Self::GRAPHICS => "graphics",
+            Self::PACKAGES => "packages",
+            Self::DISKS => "disks",
+            Self::BATTERY => "battery",
+            _ => "",
+        }
+    }
+}
+
+const DEFAULT_CONFIG: [Table; 7] = [
+    Table::SYSTEM,
+    Table::PROCESSOR,
+    Table::MEMORY,
+    Table::GRAPHICS,
+    Table::PACKAGES,
+    Table::DISKS,
+    Table::BATTERY,
 ];
-static DEFAULT_CONFIG: &str = "system,processor,graphics,memory,packages,disks,battery";
+const FULL_CONFIG: [Table; 7] = DEFAULT_CONFIG;
 const CONFIG_DIRECTORY: &str = "$HOME/.config/omnid";
 const CONFIG_FILENAME: &str = "scu";
 
+fn default_config_to_file() -> String {
+    DEFAULT_CONFIG
+        .iter()
+        .map(|table| table.to_str())
+        .filter(|table| !table.is_empty())
+        .fold(String::new(), |a, b| a + b + " ")
+        .trim_end()
+        .replace(" ", ",")
+}
+fn full_config() -> Vec<Table> {
+    FULL_CONFIG.into_iter().collect()
+}
+
 pub struct Config {
-    pub order: Vec<String>,
+    pub order: Vec<Table>,
 }
 
 impl Config {
@@ -35,20 +92,26 @@ impl Config {
             order: Self::load(),
         }
     }
-    fn load() -> Vec<String> {
-        let default = DEFAULT_CONFIG
-            .split(",")
-            .map(|s| s.to_string())
-            .collect::<Vec<String>>();
+    fn load() -> Vec<Table> {
+        let default = DEFAULT_CONFIG.into_iter().collect::<Vec<Table>>();
         if let Some(cfg_path) = Self::init() {
             if let Ok(cfg_content) = fs::read_to_string(cfg_path) {
-                let order: Vec<String> = cfg_content
+                let config: Vec<String> = cfg_content
                     .replace(" ", "")
                     .trim()
                     .split(",")
-                    .filter(|info_table| ALL_TABLES.contains(info_table))
                     .map(|s| s.to_string())
                     .collect();
+
+                let order: Vec<Table> = if config.contains(&"full".to_string()) {
+                    full_config()
+                } else {
+                    config
+                        .iter()
+                        .map(|table| Table::from_str(&table))
+                        .flatten()
+                        .collect()
+                };
 
                 if !order.is_empty() {
                     return order;
@@ -76,7 +139,8 @@ impl Config {
 
                 if full_path.join(CONFIG_FILENAME).exists()
                     || (Self::create_config_directory(&full_path)
-                        && fs::write(full_path.join(CONFIG_FILENAME), DEFAULT_CONFIG).is_ok())
+                        && fs::write(full_path.join(CONFIG_FILENAME), default_config_to_file())
+                            .is_ok())
                 {
                     return Some(full_path);
                 }
