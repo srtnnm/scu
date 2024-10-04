62079aa870538fad9a995c574084a5ef84dc0665
sk44rt <a641557@gmail.com>
2024-10-04 16:38:46 +0200

    remove clap for argument parsing (-300KiB in release build man!!!)

--- src/about.rs
+++ src/about.rs
@@ -1,2 +1,4 @@
+pub const BIN_NAME: &str = env!("CARGO_BIN_NAME");
+pub const VERSION: &str = env!("CARGO_PKG_VERSION");
 pub const DESCRIPTION: &str =
     "[Omnid]/scu is a command line system info fetch utility is aimed at informativeness";
--- src/args.rs
+++ src/args.rs
@@ -1,31 +1,74 @@
 use crate::about;
 
-use clap::Parser;
 use libscu::util::platform::unix::libc::isatty;
 
-#[derive(Parser, Debug)]
-#[command(version, about, long_about = Some(about::DESCRIPTION))]
+#[derive(Default)]
 pub(super) struct Args {
-    /// Outputs information in a much simpler form, forced by default when output is piped
-    #[arg(long, default_value_t = false)]
+    // Outputs information in a much simpler form, forced by default when output is piped
     pub simplify: bool,
 
-    /// Outputs information in regular form, even if it's piped (disables --simplify)
-    #[arg(long, default_value_t = false)]
+    // Outputs information in regular form, even if it's piped (disables --simplify)
     pub ignore_pipe: bool,
 
-    /// Enables version fetching for WMs (it was disabled by default due to bad performance on some WMs)
-    #[arg(long, default_value_t = false)]
+    // Enables version fetching for WMs (it was disabled by default due to bad performance on some WMs)
     pub force_versions: bool,
 
-    /// Show raw models without processing
-    #[arg(long, default_value_t = false)]
+    // Show raw models without processing
     pub raw_models: bool,
 }
 
+fn version() {
+    println!("{} v{}", about::BIN_NAME, about::VERSION);
+    println!("libscu v{}", libscu::VERSION);
+
+    std::process::exit(0);
+}
+
+const ARGS_WITH_DESCRIPTION: [(&str,&str);6] = [
+    ("--simplify","Outputs information in a much simpler form, forced by default when output is piped"),
+    ("--ignore-pipe", "Outputs information in regular form, even if it's piped (disables --simplify)"),
+    ("--force-versions", "Enables version fetching for WMs (it was disabled by default due to bad performance on some WMs)"),
+    ("--raw-models", "Show raw models without processing"),
+    ("--version", "Print version and exit."),
+    ("--help", "Print this page and exit"),
+];
+
+fn help() {
+    println!("{}", about::DESCRIPTION);
+
+    if !ARGS_WITH_DESCRIPTION.is_empty() {
+        println!("\nOptions:");
+        let max_option_name = ARGS_WITH_DESCRIPTION
+            .iter()
+            .map(|(option, _)| option.len())
+            .max()
+            .unwrap_or_default();
+        for (option, description) in ARGS_WITH_DESCRIPTION {
+            println!(
+                "    {option}{spacing}  {description}",
+                spacing = " ".repeat(max_option_name.saturating_sub(option.len()))
+            )
+        }
+    }
+
+    std::process::exit(0);
+}
+
 pub(super) fn arg_parse() -> Args {
-    let mut parsed_args = Args::parse();
-    parsed_args.simplify =
-        !parsed_args.ignore_pipe && (unsafe { isatty(0) == 0 } || parsed_args.simplify);
-    parsed_args
+    let env_args = std::env::args().collect::<Vec<String>>();
+    let mut args = Args::default();
+
+    if env_args.contains(&"--version".to_string()) {
+        version();
+    } else if env_args.contains(&"--help".to_string()) {
+        help();
+    }
+
+    args.force_versions = env_args.contains(&"--force-versions".to_string());
+    args.simplify = env_args.contains(&"--simplify".to_string());
+    args.ignore_pipe = env_args.contains(&"--ignore-pipe".to_string());
+    args.raw_models = env_args.contains(&"--raw-models".to_string());
+
+    args.simplify = !args.ignore_pipe && (unsafe { isatty(0) == 0 } || args.simplify);
+    args
 }
