420b1bc4e30f8cb5c04003ede9ffac3b0f8ab7e8
Artem <a641557@gmail.com>
2025-01-10 04:58:00 +0100

    add --neomimic argument

--- src/args.rs
+++ src/args.rs
@@ -18,6 +18,9 @@ pub(super) struct Args {
 
     // Show multiple cpus instead of single cpu (UNSTABLE!)
     pub multicpu: bool,
+
+    // Mimic the legendary neofetch (forces --raw-models)
+    pub neomimic: bool,
 }
 
 fn version() {
@@ -27,12 +30,13 @@ fn version() {
     std::process::exit(0);
 }
 
-const ARGS_WITH_DESCRIPTION: [(&str,&str);7] = [
+const ARGS_WITH_DESCRIPTION: [(&str,&str); 8] = [
     ("--simplify","Outputs information in a much simpler form, forced by default when output is piped."),
     ("--ignore-pipe", "Outputs information in regular form, even if it's piped. (disables --simplify)"),
     ("--force-versions", "Enables version fetching for WMs. (it was disabled by default due to bad performance on some WMs)"),
     ("--raw-models", "Show raw models without processing."),
     ("--multicpu", "Show multiple cpus instead of single cpu. (UNSTABLE!)"),
+    ("--neomimic", "Mimic the legendary neofetch (forces --raw-models)"),
     ("--version", "Print version and exit."),
     ("--help", "Print this page and exit."),
 ];
@@ -75,6 +79,10 @@ pub(super) fn arg_parse() -> Args {
     args.ignore_pipe = env_args.contains(&"--ignore-pipe".to_string());
     args.raw_models = env_args.contains(&"--raw-models".to_string());
     args.multicpu = env_args.contains(&"--multicpu".to_string());
+    args.neomimic = env_args.contains(&"--neomimic".to_string());
+    if args.neomimic {
+        args.raw_models = true;
+    }
 
     args.simplify = !args.ignore_pipe && (output_is_piped() || args.simplify);
     args
