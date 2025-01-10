e432e39061086c0e69dd182eef9d3dec790f97a0
Artem <a641557@gmail.com>
2025-01-10 04:24:46 +0100

    add NeoMimic Mode to src/display/mod.rs

--- src/display/mod.rs
+++ src/display/mod.rs
@@ -1,15 +1,17 @@
 mod modes;
 
-use crate::{args::Args, config, info};
+use crate::{args::Args, config, info::SystemInformation};
 
 #[derive(Default)]
 pub enum Mode {
     #[default]
     Table,
+    NeoMimic,
 }
 
-pub fn run(mode: Mode, info: &info::SystemInformation, config: &config::Config, args: Args) {
+pub fn run(mode: Mode, info: &SystemInformation, config: &config::Config, args: Args) {
     match mode {
         Mode::Table => modes::table::run(info, config, args),
+        Mode::NeoMimic => modes::neomimic::display(info),
     }
 }
