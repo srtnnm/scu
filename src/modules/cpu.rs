3a96e63e4d66211227d847b6dc7f24f733435ee7
Artem <a641557@gmail.com>
2025-03-31 21:36:49 +0200

    remove unused structure MultiCPU

--- src/modules/cpu.rs
+++ src/modules/cpu.rs
@@ -5,7 +5,6 @@ use crate::config::{multicpu, raw_models};
 use libscu::hardware::cpu;
 
 pub struct CPU;
-pub struct MultiCPU;
 
 impl Detection for CPU {
     type Result = Vec<cpu::Unit>;
@@ -20,14 +19,5 @@ impl Detection for CPU {
         } else {
             cpu::fetch_multicpus(raw_models())
         }
-    }
-}
-
-impl Detection for MultiCPU {
-    type Result = Vec<cpu::Unit>;
-    const NAME: &'static str = "cpu";
-
-    fn fetch(&self) -> std::io::Result<Self::Result> {
-        cpu::fetch_multicpus(raw_models())
     }
 }
